11f644b133810dc617373ad6298e9ecf3facfc52
TomE <tommevans210@gmail.com>
2022-12-15 14:38:53 +0700

    second

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log('hey');
+const tabLinks = document.getElementsByClassName('tab-link');
+const allContent = document.querySelectorAll('.tab-content');
+
+for (let i = 0; i < tabLinks.length; i++) {
+  tabLinks[i].addEventListener('click', function (e) {
+    const current = document.getElementsByClassName('active');
+    current[0].className = current[0].className.replace('active', '');
+    this.className += 'active';
+  });
+}

22223d0c3370d0777dcf3e2aacf47cb2d2f436af
TomE <tommevans210@gmail.com>
2022-12-16 09:03:09 +0700

    done

--- scripts.js
+++ scripts.js
@@ -5,6 +5,17 @@ for (let i = 0; i < tabLinks.length; i++) {
   tabLinks[i].addEventListener('click', function (e) {
     const current = document.getElementsByClassName('active');
     current[0].className = current[0].className.replace('active', '');
-    this.className += 'active';
+    this.className += ' active';
+    // switch contents
+    const filter = e.target.dataset.filter;
+    console.log(filter);
+
+    allContent.forEach((content) => {
+      if (content.classList.contains(filter)) {
+        content.style.display = 'block';
+      } else {
+        content.style.display = 'none';
+      }
+    });
   });
 }
